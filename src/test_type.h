27317f1762c47aa1a46d1b0e5aae402c4c6c4c19
王虎 <wanghu64931@goldwind.com>
2025-03-07 14:37:17 +0800

    first edition

---
+++ src/test_type.h
@@ -0,0 +1,30 @@
+﻿#ifndef __TEST_TYPE_H__
+#define __TEST_TYPE_H__
+
+#include <iostream>
+
+// 四元数 符合内存对齐
+struct quat {
+  double e0;
+  double e1;
+  double e2;
+  double e3;
+
+  void Show() { std::cout << e0 << "\n"; }
+};
+
+// 三维坐标 不符合内存对齐
+struct pos {
+  double e0;
+  double e1;
+  double e2;
+};
+
+// 三维坐标 手动内存对齐
+struct alignas(32) posSIMD {
+  double e0;
+  double e1;
+  double e2;
+};
+
+#endif  // __TEST_TYPE_H__
