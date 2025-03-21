62c98569eabb1b94870c6314cdc2b05bed22ba4b
64931 <www.wh1131457765wh@foxmail.com>
2025-03-21 11:28:40 +0800

    remove mkl and blas, perf not good for small data elment-wise calculation

--- src/test/test_2d.cc
+++ src/test/test_2d.cc
@@ -11,57 +11,12 @@ using std::vector;
 //
 #include "src/avx2/mdvector.h"
 #include "src/base_expr/base_expr.h"
-#include "src/blas/function.h"
 #include "src/header/normal.h"
 #include "src/header/time_cost.h"
 #include "src/highway/function.h"
-// 与blas命名冲突
-// #include "src/mkl/intel_mkl.h"
 
 //
 #include "test_set.h"
-
-template <class T>
-void test_openblas() {
-  AlignedAllocator<T> allocator_;
-
-  T* data1_ = allocator_.allocate(total_element);
-  T* data2_ = allocator_.allocate(total_element);
-  T* data3_ = allocator_.allocate(total_element);
-  T* data4_ = allocator_.allocate(total_element);
-
-  // 赋值
-  for (size_t i = 0; i < total_element; i++) {
-    data1_[i] = 1;
-    data2_[i] = 2;
-    data4_[i] = 4;
-  }
-
-  TimerRecorder a("blas 1d");
-
-  size_t k = 0;
-  while (k++ < loop) {
-    if constexpr (do_add) {
-      blas_add(data1_, data2_, data3_, total_element);
-    }
-
-    if constexpr (do_sub) {
-      blas_sub(data1_, data2_, data3_, total_element);
-    }
-
-    if constexpr (do_mul) {
-      blas_mul(data1_, data2_, data3_, total_element);
-    }
-
-    if constexpr (do_div) {
-      blas_div(data1_, data2_, data3_, total_element);
-    }
-  }
-
-  allocator_.deallocate(data1_);
-  allocator_.deallocate(data2_);
-  allocator_.deallocate(data3_);
-}
 
 template <class T>
 void test_norm() {
@@ -499,52 +454,12 @@ void test_xtensor() {
   }
 }
 
-template <class T>
-void test_mkl_avx2(size_t total_element) {
-  MklAlignedAllocator<T> allocator_;
-  T* data1_ = allocator_.allocate(total_element);
-  T* data2_ = allocator_.allocate(total_element);
-  T* data3_ = allocator_.allocate(total_element);
-
-  // 赋值
-  for (size_t i = 0; i < total_element; i++) {
-    data1_[i] = 1;
-    data2_[i] = 2;
-  }
-
-  TimerRecorder a(std::string(typeid(T).name()) + ": mkl");
-
-  size_t k = 0;
-  while (k++ < loop) {
-    if constexpr (do_add) {
-      mkl_add<T>(data1_, data2_, data3_, total_element);
-    }
-
-    if constexpr (do_sub) {
-      avx2_sub<T>(data1_, data2_, data3_, total_element);
-    }
-
-    if constexpr (do_mul) {
-      avx2_mul<T>(data1_, data2_, data3_, total_element);
-    }
-
-    if constexpr (do_div) {
-      avx2_div<T>(data1_, data2_, data3_, total_element);
-    }
-  }
-
-  allocator_.deallocate(data1_);
-  allocator_.deallocate(data2_);
-  allocator_.deallocate(data3_);
-}
-
 int main(int args, char* argv[]) {
   // set_mkl_avx2_sequential_mode();
 
   std::cout << "2d matrix ? matrix: " << dim1 << "*" << dim2 << "\n";
 
   // double
-  test_openblas<double>();
   test_eigen_matrixd();
   test_highway<double>();
   test_avx2<double>();
@@ -555,7 +470,6 @@ int main(int args, char* argv[]) {
   test_xtensor<double>();
   test_vector<double>();
   test_norm<double>();
-  // test_mkl_avx2<double>();
 
   std::cout << "test complete" << std::endl;
 
