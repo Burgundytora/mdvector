501ca3ba12d6e7b5680516b95e722e1c5b5c52a8
王虎 <www.wh1131457765wh@foxmail.com>
2025-07-22 14:10:39 +0800

    整理

--- src/mdvector/simd/simd_base.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MDVECTOR_SIMD_BASE_H__
-#define __MDVECTOR_SIMD_BASE_H__
-
-// ======================== SIMD配置 ========================
-template <typename T>
-struct simd;
-
-#endif  // __SIMD_BASE_H__
